1e5bdbfee91e1ae38dc2ebd50d3ed75ffdd7a143
Alvin Hong <92331387+alvin-hong@users.noreply.github.com>
2021-11-08 14:15:29 -0500

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sensors',
+        url: './sensors',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Readings',
+        url: './readings',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Alert',
+        url: './alert',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: './about',
+        cName: 'nav-links'
+    },
+]
