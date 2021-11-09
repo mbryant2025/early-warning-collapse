b39df123596d3db17c586d4a15c71ccea53ca2ec
Michael Bryant <76832019+mbryant2025@users.noreply.github.com>
2021-11-09 14:10:06 -0500

    Add files via upload

--- AR/compareModels.py
+++ AR/compareModels.py
@@ -1,55 +1,48 @@
 import numpy as np
 import glob, os
 import pickle
+from runAR import runModelFromArray
+from ARfromJSON import loadData
 
-#How many datapoints the model looks back to determine "fit" of model
-timescale = 20
+#Returns the model in modelDirectory that most accurately fits the JSON file at pathname logs
+def compare(logs, sensorUnit, sensor, modelDirectory):
 
-#Temp random data
-data = np.random.randint(800, 900, size = 20)
+	#How many datapoints the model looks back to determine "fit" of model
+	#PRECONDITION: timescale >= 2
+	timescale = 20
 
-#Returns r^2 value of two datasets
-def r2(actual, predicted):
-    corr_matrix = np.corrcoef(actual, predicted)
-    corr = corr_matrix[0,1]
-    return corr**2
- 
-models = []
-#Models stored locally for time being,
-#Will be pulled from cloud in the future
-os.chdir("/Users/michaelbryant/Desktop/ar")
-for file in glob.glob("*.obj"):
-    models.append(file)
+	#Returns r^2 value of two datasets
+	def r2(actual, predicted):
+		corr_matrix = np.corrcoef(actual, predicted)
+		corr = corr_matrix[0,1]
+		return corr**2
 
+	#Calculate predictions from model for previous timsecale data points
+	def determinePredictions(model, data):
+		predictions = []
+		for i in range(len(data) - 2):
+			slice = data[i:i+2]
+			predictions.append(runModelFromArray(slice, model))
+		return predictions
 
-window = 2
+	models = []
+	#Models stored locally for time being
+	#Will be pulled from cloud in the future
+	os.chdir(modelDirectory)
+	for file in glob.glob("*.obj"):
+		models.append(file)
 
-#load model
-file_to_read = open("model_fit.obj", "rb")
-model_fit = pickle.load(file_to_read)
-file_to_read.close()
-coef = model_fit.params
+	readings = loadData(logs, sensorUnit, sensor)
 
-# walk forward over time steps in test
-history = data[len(data)-window:]
-history = [history[i] for i in range(len(history))]
-predictions = list()
-for t in range(len(data)):
-	length = len(history)
-	lag = [history[i] for i in range(length-window,length)]
-	yhat = coef[0]
-	for d in range(window):
-		yhat += coef[d+1] * lag[window-d-1]
-	obs = data[t]
-	predictions.append(yhat)
-	history.append(obs)
+	if len(readings) > timescale:
+		readings = readings[-timescale:]
 
+	if len(readings) < 2:
+		raise ValueError("timescale must be >= 3")
 
-max = (None, 0)
-for model in models:
-    r2 = r2(data, predictions)
-    if r2 > max[1]:
-        max = (model, r2)
+	modelScores = {}
 
-print(model)
-print(max[1])
+	for model in models:
+		modelScores[model] = r2(readings[2:], determinePredictions(model, readings))
+
+	return max(modelScores, key=modelScores.get)
