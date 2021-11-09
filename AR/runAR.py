8596aabb0c58650d6ba1e109d12277d68a53d943
Michael Bryant <76832019+mbryant2025@users.noreply.github.com>
2021-11-09 13:33:11 -0500

    Add files via upload

--- AR/runAR.py
+++ AR/runAR.py
@@ -2,28 +2,28 @@ from statsmodels.tsa.ar_model import AutoReg
 import pickle
 from ARfromJSON import loadData
 
-#number of previous data points loaded
-backtrack = 150
+def runModel():
+	#number of previous data points loaded
+	backtrack = 150
 
-#load dataset
-series = loadData("Proto3.json", "ESP1", "FlexSensor")
-prev = series[-backtrack:]
+	#load dataset
+	series = loadData("Proto3.json", "ESP1", "FlexSensor")
+	prev = series[-backtrack:]
 
-#load model
-file_to_read = open("model_fit.obj", "rb")
-model_fit = pickle.load(file_to_read)
-file_to_read.close()
-coef = model_fit.params
+	#load model
+	file_to_read = open("model_fit.obj", "rb")
+	model_fit = pickle.load(file_to_read)
+	file_to_read.close()
+	coef = model_fit.params
 
-#run through model
-window = 2
-predictions = list()
-length = len(prev)
-lag = [prev[i] for i in range(length-window,length)]
-yhat = coef[0]
-for d in range(window):
-	yhat += coef[d+1] * lag[window-d-1]
-prediction = yhat
+	#run through model
+	window = 2
+	predictions = list()
+	length = len(prev)
+	lag = [prev[i] for i in range(length-window,length)]
+	yhat = coef[0]
+	for d in range(window):
+		yhat += coef[d+1] * lag[window-d-1]
+	prediction = yhat
 
-# output
-print(prediction)
+	return prediction
