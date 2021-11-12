3c5e354475eb1642e340ac58bf88778361a82e5d
Michael Bryant <76832019+mbryant2025@users.noreply.github.com>
2021-11-12 14:08:41 -0500

    Add files via upload

--- AR/runAR.py
+++ AR/runAR.py
@@ -2,16 +2,16 @@ from statsmodels.tsa.ar_model import AutoReg
 import pickle
 from ARfromJSON import loadData
 
-def runModel():
+def runModel(data, model, sensorUnit, sensor):
 	#number of previous data points loaded
 	backtrack = 150
 
 	#load dataset
-	series = loadData("Proto3.json", "ESP1", "FlexSensor")
+	series = loadData(data, sensorUnit, sensor)
 	prev = series[-backtrack:]
 
 	#load model
-	file_to_read = open("model_fit.obj", "rb")
+	file_to_read = open(model, "rb")
 	model_fit = pickle.load(file_to_read)
 	file_to_read.close()
 	coef = model_fit.params
@@ -27,3 +27,29 @@ def runModel():
 	prediction = yhat
 
 	return prediction
+
+def runModelFromArray(data, model):
+	#number of previous data points loaded
+	backtrack = 150
+
+	#load dataset
+	prev = data[-backtrack:]
+
+	#load model
+	file_to_read = open(model, "rb")
+	model_fit = pickle.load(file_to_read)
+	file_to_read.close()
+	coef = model_fit.params
+
+	#run through model
+	window = 2
+	predictions = list()
+	length = len(prev)
+	lag = [prev[i] for i in range(length-window,length)]
+	yhat = coef[0]
+	for d in range(window):
+		yhat += coef[d+1] * lag[window-d-1]
+	prediction = yhat
+
+	return prediction
+
